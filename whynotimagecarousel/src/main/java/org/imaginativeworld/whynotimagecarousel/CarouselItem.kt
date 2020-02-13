fec21172d67b662a4e15fcab9c4f855e26ea87fe
Md. Mahmudul Hasan Shohag <imaginativeshohag@gmail.com>
2020-02-13 17:41:08 +0600

    Small change in CarouselItem model

--- whynotimagecarousel/src/main/java/org/imaginativeworld/whynotimagecarousel/CarouselItem.kt
+++ whynotimagecarousel/src/main/java/org/imaginativeworld/whynotimagecarousel/CarouselItem.kt
@@ -3,7 +3,7 @@ package org.imaginativeworld.whynotimagecarousel
 import org.jetbrains.annotations.NotNull
 import org.jetbrains.annotations.Nullable
 
-data class CarouselItem(
+data class CarouselItem @JvmOverloads constructor(
     @NotNull val imageUrl: String,
-    @Nullable val caption: String = ""
+    @Nullable val caption: String? = null
 )
