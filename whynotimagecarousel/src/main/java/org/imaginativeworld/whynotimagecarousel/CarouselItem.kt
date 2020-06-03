7da4954f7ffbb78439011c26b6daaeb0e9e6a384
Md. Mahmudul Hasan Shohag <imaginativeshohag@gmail.com>
2020-06-03 22:00:36 +0600

    Image drawable support added

--- whynotimagecarousel/src/main/java/org/imaginativeworld/whynotimagecarousel/CarouselItem.kt
+++ whynotimagecarousel/src/main/java/org/imaginativeworld/whynotimagecarousel/CarouselItem.kt
@@ -1,9 +1,20 @@
 package org.imaginativeworld.whynotimagecarousel
 
-import org.jetbrains.annotations.NotNull
-import org.jetbrains.annotations.Nullable
+import androidx.annotation.DrawableRes
 
-data class CarouselItem @JvmOverloads constructor(
-    @NotNull val imageUrl: String,
-    @Nullable val caption: String? = null
-)
+data class CarouselItem constructor(
+    val imageUrl: String? = null,
+    @DrawableRes val imageDrawable: Int? = null,
+    val caption: String? = null
+) {
+    constructor(imageUrl: String? = null) : this(imageUrl, null, null)
+    constructor(@DrawableRes imageDrawable: Int? = null) : this(null, imageDrawable, null)
+
+    constructor(imageUrl: String? = null, caption: String? = null) : this(imageUrl, null, caption)
+    constructor(@DrawableRes imageDrawable: Int? = null, caption: String? = null) : this(
+        null,
+        imageDrawable,
+        caption
+    )
+
+}
