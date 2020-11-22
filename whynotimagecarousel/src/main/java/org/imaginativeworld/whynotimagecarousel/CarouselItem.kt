7d8142eb046bdf45a331c7b83901cd2c22822e3e
Md. Mahmudul Hasan Shohag <imaginativeshohag@gmail.com>
2020-11-23 00:42:59 +0600

    Libraries updated, unnecessary files removed, library re-published in maven central

--- whynotimagecarousel/src/main/java/org/imaginativeworld/whynotimagecarousel/CarouselItem.kt
+++ whynotimagecarousel/src/main/java/org/imaginativeworld/whynotimagecarousel/CarouselItem.kt
@@ -7,14 +7,24 @@ data class CarouselItem constructor(
     @DrawableRes val imageDrawable: Int? = null,
     val caption: String? = null
 ) {
-    constructor(imageUrl: String? = null) : this(imageUrl, null, null)
-    constructor(@DrawableRes imageDrawable: Int? = null) : this(null, imageDrawable, null)
-
-    constructor(imageUrl: String? = null, caption: String? = null) : this(imageUrl, null, caption)
+    constructor(imageUrl: String? = null) : this(
+        imageUrl,
+        null,
+        null
+    )
+    constructor(@DrawableRes imageDrawable: Int? = null) : this(
+        null,
+        imageDrawable,
+        null
+    )
+    constructor(imageUrl: String? = null, caption: String? = null) : this(
+        imageUrl,
+        null,
+        caption
+    )
     constructor(@DrawableRes imageDrawable: Int? = null, caption: String? = null) : this(
         null,
         imageDrawable,
         caption
     )
-
 }
